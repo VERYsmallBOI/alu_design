cde4ccefe43f4d51bda9a83ea731b46a14704e52
VERYsmallBOI <tamilselvanelango3@gmail.com>
2026-05-05 20:20:27 +0530

    commit=6

--- alu.v
+++ alu.v
@@ -2,9 +2,9 @@ module alu #(
     parameter width = 8,
     cwidth = 4
 ) (
-    clk, rst, inp_valid, mode, cmd, ce, opa, opb, cin, err, result, offlow, cout, g, l, e
+    clk, rst, inp_valid, mode, cmd, ce, opa, opb, cin, err, res, offlow, cout, g, l, e
 );
-    //change after finishing all result to res
+    //change after finishing all res to res
     input wire clk, rst, mode, ce, cin;
     input wire [1:0]inp_valid;
     input wire [width-1:0] opa, opb;
@@ -12,20 +12,20 @@ module alu #(
     output reg g, l, e;
     output wire cout, offlow;
     output reg err;
-    output reg [2*width-1:0] result;
+    output reg [2*width-1:0] res;
 
     reg [2*width-1:0] res1;
 
     //cout for unsgined + and overflow for everyother
     //0 is driver default not z
 
-reg mg;//multiplication going on
+reg mg;//
 
     reg [1:0] count;
 
     always @(posedge clk or posedge rst) begin
         if (rst) begin
-            result <= 0;
+            res <= 0;
             g      <= 0;
             l      <= 0;
             e      <= 0;
@@ -36,28 +36,28 @@ reg mg;//multiplication going on
                 g      <= 0;
                 l      <= 0;
                 e      <= 0;
-                result <= 0;
+                res <= 0;
                 res1<=0;
                 count<=(cmd==4'd10||cmd==4'd9)?count:0;
                 mg     <= (count!=0)?mg:0;
                 if (mode) begin
                     case (cmd)
                         0: begin
-                            result <= opa + opb;
+                            res <= opa + opb;
                         end
                         1: begin
-                            result <= opa - opb;
+                            res <= opa - opb;
                         end
                         2: begin
-                            result <= cin + opa + opb;
+                            res <= cin + opa + opb;
                         end
                         3: begin
-                            result <= opa - opb - cin;
+                            res <= opa - opb - cin;
                         end
-                        4: result <= opa + 1'b1;
-                        5: result <= opa - 1'b1;
-                        6: result <= opb + 1'b1;
-                        7: result <= opb - 1'b1;
+                        4: res <= opa + 1'b1;
+                        5: res <= opa - 1'b1;
+                        6: res <= opb + 1'b1;
+                        7: res <= opb - 1'b1;
                         8: begin
                             g <= (opa > opb);
                             l <= (opa < opb);
@@ -67,7 +67,7 @@ reg mg;//multiplication going on
                         begin
                         if(count>=2'd2)
                         begin
-                            result<=res1;
+                            res<=res1;
                             count<=0;
                             
                         end
@@ -83,7 +83,7 @@ reg mg;//multiplication going on
                         begin
                         if(count>=2'd2)
                         begin
-                            result<=res1;
+                            res<=res1;
                             count<=0;
                             
                         end
@@ -100,34 +100,34 @@ reg mg;//multiplication going on
                             g <= (opa > opb);
                             l <= (opa < opb);
                             e <= (opa == opb);
-                            result <= $signed(opa) + $signed(opb);
+                            res <= $signed(opa) + $signed(opb);
                         end
                         
                         12:  begin
                             g <= (opa > opb);
                             l <= (opa < opb);
                             e <= (opa == opb);
-                            result <= $signed(opa) - $signed(opb);
+                            res <= $signed(opa) - $signed(opb);
                         end
-                        default: result <= 0;
+                        default: res <= 0;
                     endcase
                 end else begin
                     case (cmd)
-                        0:       result <= opa & opb;
-                        1:       result <= ~(opa & opb);
-                        2:       result <= opa | opb;
-                        3:       result <= ~(opa | opb);
-                        4:       result <= opa ^ opb;
-                        5:       result <= (opa ~^ opb);
-                        6:       result <= ~opa;
-                        7:       result <= ~opb;
-                        8:       result <= opa >> 1;
-                        9:       result <= opa << 1;
-                        10:      result <= opb >> 1;
-                        11:      result <= opb << 1;
-                        12:      result <= (((1 << width) - 1) & ((opa >> (width - opb[$clog2(width):0])) | (opa << (opb[$clog2(width):0]))));
-                        13:      result <= (((1 << width) - 1) & ((opa << (width - opb[$clog2(width):0])) | (opa >> (opb[$clog2(width):0]))));
-                        default: result <= 0;
+                        0:       res <= opa & opb;
+                        1:       res <= ~(opa & opb);
+                        2:       res <= opa | opb;
+                        3:       res <= ~(opa | opb);
+                        4:       res <= opa ^ opb;
+                        5:       res <= (opa ~^ opb);
+                        6:       res <= ~opa;
+                        7:       res <= ~opb;
+                        8:       res <= opa >> 1;
+                        9:       res <= opa << 1;
+                        10:      res <= opb >> 1;
+                        11:      res <= opb << 1;
+                        12:      res <= (((1 << width) - 1) & ((opa >> (width - opb[$clog2(width):0])) | (opa << (opb[$clog2(width):0]))));
+                        13:      res <= (((1 << width) - 1) & ((opa << (width - opb[$clog2(width):0])) | (opa >> (opb[$clog2(width):0]))));
+                        default: res <= 0;
                     endcase
                 end
             end
@@ -135,15 +135,15 @@ reg mg;//multiplication going on
     end
 
     assign offlow = rst ? 0 : (
-        ((mode & (cmd == 11)) & (opa[width-1] == opb[width-1]) & (result[width-1] != (opa[width-1]))) ||
-        ((mode & (cmd == 12)) & (opa[width-1] != opb[width-1]) & (result[width-1] != (opa[width-1]))) ||
+        ((mode & (cmd == 11)) & (opa[width-1] == opb[width-1]) & (res[width-1] != (opa[width-1]))) ||
+        ((mode & (cmd == 12)) & (opa[width-1] != opb[width-1]) & (res[width-1] != (opa[width-1]))) ||
         ((mode & (cmd == 1)) & (opb > opa)) ||
         ((mode & (cmd == 3)) & ((opb + cin) > opa))
     );
 
     assign cout = rst ? 0 : (
-        (mode & (cmd == 0) & (result[width])) ||
-        (mode & (cmd == 2) & (result[width]))
+        (mode & (cmd == 0) & (res[width])) ||
+        (mode & (cmd == 2) & (res[width]))
     );
 
     always@(posedge clk,posedge rst)
